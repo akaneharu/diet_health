5675b2081a018b82698de03c2962074e1c43674d
EC2 Default User <ec2-user@ip-172-31-86-206.ec2.internal>
2024-06-06 04:22:10 +0000

    プロフィール画面

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'users/edit'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :edit, :update]
+
   root to: 'homes#top'
-  get '/about' => 'homes#about'
+  get '/about' => 'homes#about', as: 'about'
+
 end
